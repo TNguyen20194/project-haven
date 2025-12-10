79481c716db76d3e3fe92b35e787b75fbf40dced
Truong Nguyen <truong.nguyen201@gmail.com>
2025-12-09 23:57:37 -0500

    Update content

--- Project/main.js
+++ Project/main.js
@@ -1,1 +1,41 @@
-console.log("main.js connected!")
+const header = document.querySelector("header");
+const mobileMenu = document.getElementById("mobile-menu");
+const toggleButtons = document.querySelectorAll(".mobile-menu-toggle");
+
+
+console.log(header, mobileMenu, toggleButtons.length)
+
+// if (!header || !mobileMenu || toggleButtons.length === 0) return;
+
+function openMenu() {
+    header.classList.add("header--menu-open");
+    mobileMenu.classList.add("mobile-menu--open");
+    mobileMenu.setAttribute("aria-hidden", "false");
+
+    toggleButtons.forEach(button => {
+        button.setAttribute("aria-expanded", "true");
+    })
+};
+
+function closeMenu() {
+    header.classList.remove("header--menu-open");
+    mobileMenu.classList.remove("mobile-menu--open");
+    mobileMenu.setAttribute("aria-hidden", "true");
+
+    toggleButtons.forEach(button => {
+        button.setAttribute("aria-expanded", "false");
+    });
+
+    document.body.style.overflow = "";
+};
+
+function toggleMenu() {
+    const isOpen = mobileMenu.classList.contains("mobile-menu--open");
+    isOpen ? closeMenu() : openMenu();
+};
+
+toggleButtons.forEach(button => {
+    button.addEventListener("click", toggleMenu)
+})
+
+
